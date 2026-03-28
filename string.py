953edb353287fbca60223885f95019776b86b462
Sriyansh2011 <iamthegoat1016@gmail.com>
2026-03-28 13:12:04 +0300

    Add files via upload

---
+++ string.py
@@ -0,0 +1,14 @@
+first_name="Codingal"
+last_name="Educations"
+full_name=first_name+last_name
+example="Haa"*5
+print(first_name)
+print(last_name)
+print(full_name)
+print(example)
+
+word='Coding'
+print(len(word))
+print(word[0])
+print(word[5])
+print(word[0:3])
