d95201219d7ede9c0cbe8b1379e0ba4e1ba4b1b5
Sriyansh2011 <iamthegoat1016@gmail.com>
2026-04-18 12:22:25 +0300

    Add files via upload

---
+++ tuple to list.py
@@ -0,0 +1,9 @@
+my_tuple = (10, 20, 30, 40)
+
+print("Original tuple:", my_tuple)
+print("Type of my_tuple:", type(my_tuple))
+
+my_list = list(my_tuple)
+
+print("Converted list:", my_list)
+print("Type of my_list:", type(my_list))
