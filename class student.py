d0e5cc04fdcb1d63d54cfb13f73fdbab3e2bb19c
Sriyansh2011 <iamthegoat1016@gmail.com>
2026-04-18 13:08:18 +0300

    Add files via upload

---
+++ class student.py
@@ -0,0 +1,13 @@
+class student:
+    grade = 10
+    name = "Penguin"
+
+    def introduction(self):
+        print("Hi I am a student")
+    def details(self):
+        print("My name is", self.name)
+        print("I Study in Grade", self.grade)
+
+ob = student()
+ob.introduction()
+ob.details()
---
+++ parrot.py
@@ -0,0 +1,29 @@
+class Parrot:
+    species = "bird"
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+blu = Parrot("Blu", 10)
+woo = Parrot("Woo", 15)
+
+print("Blu is a {}".format(blu.species))
+print("Woo is also a {}".format(woo.species))
+
+print("{} is {} years old.".format(blu.name,blu.age))
+print("{} is {} years old".format(woo.name,woo.age))
+
+class Parrot2:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def sing(self,song):
+        return "{} sings {}".format(self.name, song)
+    def dance(self):
+        return "{} is now dancing".format(self.name)
+    
+blu = Parrot2("Blu", 10)
+print(blu.sing("'Happy'"))
+print(blu.dance())
