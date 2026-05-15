b64a8fb8125e857aecbabe7546330ee123c0d69b
Sriyansh2011 <iamthegoat1016@gmail.com>
2026-05-15 13:09:08 +0300

    Add files via upload

---
+++ square area.py
@@ -0,0 +1,10 @@
+class square:
+    def __init__(self):
+        self.__side = 10
+    def area(self):
+        print("side :", self.__side)
+        print("My area is :", self.__side**2)
+
+ob = square()
+ob.__side = 15
+ob.area()
