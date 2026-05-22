59927dac6dde3d9772770bd70c369fc752e6b0b8
Sriyansh2011 <iamthegoat1016@gmail.com>
2026-05-22 17:46:17 +0300

    Add files via upload

---
+++ polygons oop.py
@@ -0,0 +1,81 @@
+from abc import ABC, abstractmethod
+
+class Polygon(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Polygon):
+
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+
+    def area(self):
+        return self.length * self.breadth
+
+class Square(Polygon):
+
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side * self.side
+
+class Triangle(Polygon):
+
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return 0.5 * self.base * self.height
+
+class Paralellogram(Polygon):
+
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return self.base * self.height
+
+
+print("Choose Polygon")
+print("1. Rectangle")
+print("2. Square")
+print("3. Triangle")
+print("4. Paralellogram")
+
+choice = int(input("Enter choice: "))
+
+if choice == 1:
+    l = float(input("Enter length: "))
+    b = float(input("Enter breadth: "))
+
+    obj = Rectangle(l, b)
+    print("Area of Rectangle =", obj.area())
+
+elif choice == 2:
+    s = float(input("Enter side: "))
+
+    obj = Square(s)
+    print("Area of Square =", obj.area())
+
+elif choice == 3:
+    base = float(input("Enter base: "))
+    height = float(input("Enter height: "))
+
+    obj = Triangle(base, height)
+    print("Area of Triangle =", obj.area())
+
+elif choice == 4:
+    base = float(input("Enter base: "))
+    height = float(input("Enter height: "))
+
+    obj = Paralellogram(base, height)
+    print("Area of Paralellogram =", obj.area())
+
+else:
+    print("Invalid Choice")
